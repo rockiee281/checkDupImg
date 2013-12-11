f4e0a0b4d341885c3455f9bf45c3f0d63ad575c5
liyun <rockiee281@gmail.com>
2013-12-11 11:56:34 +0800

    first commit

--- checkDupImg.py
+++ checkDupImg.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# encoding=utf-8
 #check duplicate picture from web
 import urllib2
 import urlparse
@@ -12,31 +13,74 @@ import Image
 monkey.patch_all()
 
 def checkImage(url):
-#	urlTuple = urlparse.urlparse(url)
-#	filename = urlTuple.path[urlTuple.path.rfind('/')+1:]
-#	print urlTuple.path
-#	print filename
-#	f = file('images/'+filename,'wb')
-#	print url
-	try:
-		data = StringIO(urllib2.urlopen(url).read())
-		hash = imagehash.average_hash(Image.open(data))
-		print hash,url
-		data.close()
-	except urllib2.URLError, e:
-		print e
-#	f.write(data)
-#	f.close()
+        buffer = []
+        try:
+                data = StringIO(urllib2.urlopen(url).read())
+                hash = imagehash.average_hash(Image.open(data))
+                print hash,url
+                buffer.append((str(hash),url.strip()))
+                data.close()
+        except urllib2.URLError, e:
+            print e
+        except IOError,e:
+            print e
+        except:
+            pass
+
+        #对buffer中的数据排序
+        sorted(buffer,key=lambda item:item[1])
+        result = open('result','w',2048)
+        for val in buffer:
+            result.write(val[0] + ' ' + val[1] + '\n')
+        result.close()
 
 def checkImg():
-	url='http://cloud.dakele.com/api/gamecenter/l1/beauty?limit=40&offset=0'
-	data=urllib2.urlopen(url).read()
-	jsonData = json.loads(data)['data']
-	
-#	for imgUrl in jsonData[0::2]:
-#		checkImage(imgUrl)	
-	jobs = [gevent.spawn(checkImage,imgUrl) for imgUrl in jsonData[0::2]]
-	gevent.joinall(jobs)
+        f = file('imageLists','r')
+        lines = f.readlines()
+        f.close()
+#        for imgUrl in jsonData[0::2]:
+#                checkImage(imgUrl)        
+        jobs = [gevent.spawn(checkImage,imgUrl) for imgUrl in lines]
+        gevent.joinall(jobs)
+
+def checkSimilar():
+    f = file('result')
+    outfile = file('output','w')
+    delfile = file('toBeDel','w')
+    lines = f.readlines()
+    f.close()
+
+    # if toBeDel.old exists, remove deleted images from result
+    import os 
+    if os.path.isfile('toBeDel.old'):
+        deledfile = file('toBeDel.old')
+        deledLines = deledfile.readlines()
+        deledfile.close()
+        lines = [line for line in lines if line.split(' ')[1] not in deledLines]
+    lasthash = 0
+    similar = []
+    similar.append(lines[0].strip())
+    for line in lines:
+        data = line.split(' ')
+        hash = int(data[0],16)
+        print hash
+        if hash - lasthash < 500 :
+           similar.append(line.strip())
+        else:
+            if len(similar) > 1 :
+                outfile.write(",".join(similar) + '\n')
+                for item in similar[1:]:
+                    delfile.write(item.split(' ')[1] + "\n")
+                similar = []
+                similar.append(line.strip())
+            elif len(similar) == 1:
+                similar[0] = line.strip()
+        lasthash = hash
+    outfile.close()
 
 if __name__ == '__main__':
-	checkImg()
+    import os
+    #if result has been caculated, donot process again
+    if not os.path.isfile('result'): 
+       checkImg()
+    checkSimilar()
